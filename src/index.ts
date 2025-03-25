ff9ccb87916833641e4bff9df108d69831b6637f
Ivan Efimov <chupchik4@gmail.com>
2025-03-25 21:58:00 +0300

    Work with docker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, {Request, Response} from "express";
+import mongoose from "mongoose";
+import User from "./models/User";
 
 const app = express()
 const port = 3000
@@ -7,22 +9,43 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
-app.set("view engine", "ejs")
+mongoose
+    .connect("mongodb://admin:qwerty@localhost:27017/MyContainer?authSource=admin")
+    .then(()=>console.log("Connected to MyContainer!"))
+    .catch(()=>console.log("Error connecting to MyContainer!"))
 
-app.get('/ejs-pages', (req: Request, res: Response) => {
-    const user = req.query.user || 'Гость';
-     res.render('layout', {message: "Переданное сообщение.", title: "My page", content: "index", user: user})
+app.get('/get-users', async (req: Request, res: Response) => {
+    try {
+        const users = await User.find()
+        res.status(202).json(users)
+    } catch (e) {
+        res.status(400).json({message: (e as Error).message})
+    }
 })
 
-app.get('/about', (req: Request, res: Response) => {
-    res.render('layout', {message: "Здесь должны быть контакты.", title: "About", content: "about"})
+app.post('/post-users', async (req: Request, res: Response) => {
+    try {
+        const user = new User(req.body)
+        await user.save()
+        res.status(201).json(user)
+    } catch (e) {
+        res.status(400).json({message: (e as Error).message})
+    }
 })
 
-app.get('/', (req: Request, res: Response) => {
-    console.log(req.body)
-    console.log(req.headers)
-    res.send('Hello World!')
-})
+app.delete('/delete-user/:username', async (req: Request, res: Response) => {
+    const { username } = req.params; //username из тела запроса
+
+    try {
+        const result = await User.deleteOne({ username });
+        if (result.deletedCount === 0) {
+            res.status(404).json({message:"User not found"})
+        }
+        res.status(202).json("User deleted");
+    } catch (e) {
+        res.status(400).json({ message: (e as Error).message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
